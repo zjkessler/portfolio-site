747bcdf1001cf43b9934c2a74771635c1ae1ce7f
Zach Kessler <zjkessler@gmail.com>
2017-05-04 11:12:33 -0600

    added port for heroku

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var logger = require('morgan');
+var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './src/client')));
 app.use(logger('dev'));
 
-app.listen(3000, function () {
-	console.log('Express listening on port 3000');
+app.listen(port, function () {
+	console.log('Express listening on port ' + port);
 });
