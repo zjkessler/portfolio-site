6f7df2d67b3759964d06ad4c104dda946982d133
Zach Kessler <zjkessler@gmail.com>
2017-05-11 12:42:58 -0600

    button z index fix

--- src/client/components/portfolio/portfolio.js
+++ src/client/components/portfolio/portfolio.js
@@ -5,22 +5,6 @@ app.controller('portfolioCtrl', ['MyInfoSvc', function (MyInfoSvc) {
 	var vm = this;
 	vm.content = MyInfoSvc.portfolio;
 
-	vm.hideButton = hideButton();
-	vm.showButton = showButton();
-
-
-	///////////////////////////	
-	function hideButton() {
-		$('.live-button').show();
-		console.log(event);
-	}
-
-	function showButton() {
-
-		$('.live-button').hide();
-		console.log('show button active');
-	}
-
 }]);
 app.component('portfolio', {
 	templateUrl: 'components/portfolio/portfolio.html',
